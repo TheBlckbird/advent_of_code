44e94ab4a1b0bdf05a25eabeecc57b2f3bf43fc9
TheBlckbird <theblckbrd@proton.me>
2024-08-18 20:59:15 +0200

    Add day 8 of 2015

---
+++ 2015/day_8/src/main.rs
@@ -0,0 +1,121 @@
+use std::fs::read_to_string;
+
+enum InputType {
+    Real,
+    Example,
+}
+
+fn main() {
+    let _ = calc(InputType::Real);
+}
+
+fn calc(input_type: InputType) -> (usize, usize) {
+    let mut all_chars = 0;
+    let mut real_chars = 0;
+    let mut encoded_chars = 0;
+
+    for line in parse_input(input_type) {
+        if line == "" {
+            continue;
+        }
+
+        all_chars += line.len();
+        encoded_chars += line.len() + 2;
+
+        for char in line.chars() {
+            match char {
+                '\\' => {
+                    encoded_chars += 1;
+                    println!("\\");
+                }
+                '"' => {
+                    encoded_chars += 1;
+                    println!("\"");
+                }
+                _ => {}
+            }
+        }
+
+        println!("{line}");
+        println!("{encoded_chars}");
+        println!();
+
+        let mut remaining = line;
+        let mut current_iteration = 0;
+
+        while !remaining.is_empty() {
+            current_iteration += 1;
+
+            if current_iteration == 1 || remaining.len() == 1 {
+                remaining.remove(0);
+                continue;
+            }
+
+            match remaining.chars().next().unwrap() {
+                '\\' => {
+                    remaining.remove(0);
+
+                    match remaining.chars().next().unwrap() {
+                        'x' => {
+                            remaining.remove(0);
+                            remaining.remove(0);
+                            remaining.remove(0);
+                        }
+                        '"' => {
+                            remaining.remove(0);
+                        }
+                        '\\' => {
+                            remaining.remove(0);
+                        }
+                        _ => {
+                            remaining.remove(0);
+                        }
+                    }
+
+                    real_chars += 1;
+                }
+                '"' => {
+                    break;
+                }
+                _ => {
+                    remaining.remove(0);
+                    real_chars += 1;
+                }
+            }
+        }
+    }
+
+    let part_one = all_chars - real_chars;
+    let part_two = encoded_chars - all_chars;
+
+    println!("Part 1: {part_one}");
+    println!("Part 2: {part_two}");
+
+    (part_one, part_two)
+}
+
+fn parse_input(input_type: InputType) -> Vec<String> {
+    let contents = read_to_string(match input_type {
+        InputType::Real => "./input.txt",
+        InputType::Example => "./input-example.txt",
+    })
+    .unwrap();
+    contents.split('\n').map(|line| line.to_string()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = calc(InputType::Example);
+        assert_eq!(result.0, 12);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = calc(InputType::Example);
+        assert_eq!(result.1, 19);
+    }
+}
--- year_2023/day-02/src/part1.rs
+++ year_2023/day-02/src/part1.rs
@@ -1,5 +1,91 @@
 pub fn main(input: &str) -> i64 {
-    0
+    let mut output = 0;
+
+    let lines: Vec<&str> = input.lines().collect();
+
+    // Convert each line into a 2D array of characters
+    let mut char_2d_array: Vec<Vec<char>> = Vec::new();
+    for line in lines {
+        let char_array: Vec<char> = line.chars().collect();
+        char_2d_array.push(char_array);
+    }
+
+    let mut current_number = String::new();
+
+    for (line_index, line) in char_2d_array.iter().enumerate() {
+        for (char_index, char) in line.iter().enumerate() {
+            if !char.is_numeric() {
+                continue;
+            }
+
+            current_number.push(*char);
+
+            if (line_index != char_2d_array.len()
+                && char_index != char_2d_array[line_index].len() - 1)
+                && (char_2d_array[line_index][char_index + 1].is_numeric())
+            {
+                continue;
+            }
+
+            let mut characters_around = String::with_capacity((current_number.len() + 2) * 2 + 2);
+
+            for _ in 0..current_number.chars().count() {
+                if line_index != 0 {
+                    characters_around.push(char_2d_array[line_index - 1][char_index]);
+                }
+
+                if line_index != char_2d_array.len() - 1 {
+                    characters_around.push(char_2d_array[line_index + 1][char_index]);
+                }
+            }
+
+            if char_index + 1 - current_number.len() > 0 {
+                // left
+                characters_around
+                    .push(char_2d_array[line_index][char_index - current_number.len()]);
+            }
+
+            if char_index + 1 < char_2d_array[line_index].len() {
+                // right
+                characters_around.push(char_2d_array[line_index][char_index + 1]);
+            }
+
+            if line_index > 0 && char_index + 1 - current_number.len() > 0 {
+                // top left
+                characters_around
+                    .push(char_2d_array[line_index - 1][char_index - current_number.len()]);
+            }
+
+            if line_index > 0 && char_index + 1 < char_2d_array[line_index].len() {
+                // top right
+                characters_around.push(char_2d_array[line_index - 1][char_index + 1]);
+            }
+
+            if line_index != char_2d_array.len() - 1 && char_index + 1 - current_number.len() > 0 {
+                // bottom left
+                characters_around
+                    .push(char_2d_array[line_index + 1][char_index - current_number.len()]);
+            }
+
+            if line_index != char_2d_array.len() - 1
+                && char_index + 1 < char_2d_array[line_index].len()
+            {
+                // bottom right
+                characters_around.push(char_2d_array[line_index + 1][char_index + 1]);
+            }
+
+            for character_around in characters_around.chars() {
+                if character_around != '.' {
+                    output += current_number.parse::<i64>().unwrap();
+                    break;
+                }
+            }
+
+            current_number = "".to_owned();
+        }
+    }
+
+    output
 }
 
 #[cfg(test)]
