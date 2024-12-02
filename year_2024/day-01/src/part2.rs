2bfddd1405c9cbb2c2d9b3dde020d89cb4daba76
Louis Weigel <privat@louisweigel.com>
2024-12-02 17:56:57 +0100

    Complete day 1, part 2

--- year_2024/day-01/src/part2.rs
+++ year_2024/day-01/src/part2.rs
@@ -1,5 +1,31 @@
 pub fn process(input: &str) -> String {
-    todo!()
+    let mut result = 0;
+    let parsed = parse(input);
+
+    for num in parsed.0 {
+        let occurrences = parsed
+            .1
+            .iter()
+            .filter(|&i| i == &num)
+            .collect::<Vec<&i32>>()
+            .len();
+
+        result += occurrences as i32 * num;
+    }
+
+    result.to_string()
+}
+
+fn parse(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut result = (Vec::new(), Vec::new());
+
+    for line in input.lines() {
+        let mut parts = line.split("   ");
+        result.0.push(parts.next().unwrap().parse().unwrap());
+        result.1.push(parts.next().unwrap().parse().unwrap());
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -10,7 +36,7 @@ mod tests {
     #[test]
     fn test_process() {
         let result = process(include_str!("../test_input2.txt"));
-        let expected = todo!() as String;
+        let expected = 31.to_string();
         assert_eq!(result, expected);
     }
 }
