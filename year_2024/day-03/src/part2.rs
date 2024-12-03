12dbcc127122c983d73afa78049f7f6b571d3c2d
Louis Weigel <privat@louisweigel.com>
2024-12-03 17:04:02 +0100

    Complete day 3, part 2

--- year_2024/day-03/src/part2.rs
+++ year_2024/day-03/src/part2.rs
@@ -1,5 +1,26 @@
+use regex::Regex;
+
 pub fn process(input: &str) -> String {
-    todo!()
+    let mut result: i32 = 0;
+    let mut is_do = true;
+
+    let re = Regex::new(r"don't\(\)|do\(\)|mul\((\d\d?\d?),(\d\d?\d?)\)").unwrap();
+    let captures = re.captures_iter(input);
+
+    for capture in captures {
+        let capture_type = capture.get(0).unwrap().as_str();
+
+        if capture_type == "don't()" {
+            is_do = false;
+        } else if capture_type == "do()" {
+            is_do = true;
+        } else if is_do {
+            result += capture.get(1).unwrap().as_str().parse::<i32>().unwrap()
+                * capture.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        }
+    }
+
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -10,7 +31,7 @@ mod tests {
     #[test]
     fn test_process() {
         let result = process(include_str!("../test_input2.txt"));
-        let expected = todo!().to_string();
+        let expected = 48.to_string();
         assert_eq!(result, expected);
     }
 }
