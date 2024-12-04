5a4e9b2fab0091d3f9d96afbca3e4e6ba9b24cf4
Louis Weigel <privat@louisweigel.com>
2024-12-04 19:20:10 +0100

    Remove redundant comment

--- year_2024/day-04/src/bin/part1.rs
+++ year_2024/day-04/src/bin/part1.rs
@@ -1,5 +1,4 @@
 fn main() {
-    // let input = include_str!("../../input.txt");
     let input = include_str!("../../input.txt");
     let output = day_04::part1::process(input);
     println!("Part 1: {}", output);
